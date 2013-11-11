7f0d1bc640b41e53e19eee9d0649e6ab69e28b9a
Tom Pijls <tompijls@hotmail.com>
2013-11-11 21:20:21 +0100

    Initial Commit

---
+++ BankAccount/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BankAccount
+//
+//  Created by Tom Pijls on 11/11/13.
+//  Copyright (c) 2013 Tom Pijls. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
